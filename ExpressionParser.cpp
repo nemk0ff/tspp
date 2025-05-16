4750f9d3b358a02be250083d446deaf4cb2b0a6a
Nemkoff <nemkov_work@mail.ru>
2025-05-16 23:03:04 +0300

    добавил для удобства и улучшения читаемости комментарии к методам парсера

--- ExpressionParser.cpp
+++ ExpressionParser.cpp
@@ -1,11 +1,12 @@
 #include "ExpressionParser.h"
 
+// Функция для замены всех вхождений подстроки в строке
 void replace_all(std::string &s, std::string const &toReplace, std::string const &replaceWith) {
     std::string buf;
     std::size_t pos = 0;
     std::size_t prevPos;
 
-    // Reserves rough estimate of final size of string.
+    // Резервируем память для буфера, чтобы избежать многократных перераспределений
     buf.reserve(s.size());
 
     while (true) {
@@ -23,19 +24,22 @@ void replace_all(std::string &s, std::string const &toReplace, std::string const
     s.swap(buf);
 }
 
+// Обработка сложения и вычитания
 void ExpressionParser::S() {
-    M();
+    M(); // Сначала обрабатываем умножение/деление
     while (cur_index < s.size()) {
         char c = s[cur_index];
         if (c == '+' or c == '-') {
             cur_index++;
-            M();
+            M(); // Обрабатываем следующий операнд
 
+            // Извлекаем два последних значения из стека
             value_t r = values.top();
             values.pop();
             value_t l = values.top();
             values.pop();
 
+            // Выполняем операцию и помещаем результат обратно в стек
             value_t res;
             if (c == '+') {
                 res = l + r;
@@ -50,25 +54,28 @@ void ExpressionParser::S() {
     }
 }
 
+// Обработка умножения и деления
 void ExpressionParser::M() {
-    P();
+    P(); // Сначала обрабатываем степень
     while (cur_index < s.size()) {
         char c = s[cur_index];
         if (c == '*' or c == '/') {
             cur_index++;
-            P();
+            P(); // Обрабатываем следующий операнд
 
+            // Извлекаем два последних значения из стека
             value_t r = values.top();
             values.pop();
             value_t l = values.top();
             values.pop();
 
+            // Выполняем операцию и помещаем результат обратно в стек
             value_t res;
             if (c == '*') {
                 res = l * r;
             } else {
                 if (abs(r) < EPS) {
-                    throw "divide by zero";
+                    throw "divide by zero"; // Проверка деления на ноль
                 }
                 res = l / r;
             }
@@ -80,12 +87,14 @@ void ExpressionParser::M() {
     }
 }
 
+// Обработка возведения в степень
 void ExpressionParser::P() {
-    V();
+    V(); // Сначала обрабатываем значение
     if (s[cur_index] == '^') {
         cur_index++;
-        P();
+        P(); // Рекурсивно обрабатываем степень
 
+        // Извлекаем два последних значения из стека
         value_t r = values.top();
         values.pop();
         value_t l = values.top();
@@ -96,22 +105,23 @@ void ExpressionParser::P() {
     }
 }
 
+// Обработка значений: функции, скобки, числа
 void ExpressionParser::V() {
-    if (s[cur_index] == 's') { // sin, sqrt
+    if (s[cur_index] == 's') { // Обработка функций, начинающихся на 's': sin, sqrt
         cur_index++;
-        if (s[cur_index] == 'i') {
+        if (s[cur_index] == 'i') { // sin
             if (s[++cur_index] != 'n') {
                 throw "unknown operation at index " + std::to_string(cur_index);
             }
             if (s[++cur_index] != '(') {
                 throw "parenthesis not found at index " + std::to_string(cur_index);
             }
-            V();
+            V(); // Рекурсивно обрабатываем аргумент функции
 
             value_t x = values.top();
             values.pop();
             values.push(sin(x));
-        } else if (s[cur_index] == 'q') {
+        } else if (s[cur_index] == 'q') { // sqrt
             if (s[++cur_index] != 'r') {
                 throw "unknown operation at index " + std::to_string(cur_index);
             }
@@ -121,7 +131,7 @@ void ExpressionParser::V() {
             if (s[++cur_index] != '(') {
                 throw "parenthesis not found at index " + std::to_string(cur_index);
             }
-            V();
+            V(); // Рекурсивно обрабатываем аргумент функции
 
             value_t x = values.top();
             values.pop();
@@ -129,154 +139,32 @@ void ExpressionParser::V() {
         } else {
             throw "unknown operation at index " + std::to_string(cur_index);
         }
-    } else if (s[cur_index] == 'c') { // cos, ctg
-        cur_index++;
-        if (s[cur_index] == 'o') {
-            if (s[++cur_index] != 's') {
-                throw "unknown operation at index " + std::to_string(cur_index);
-            }
-            if (s[++cur_index] != '(') {
-                throw "parenthesis not found at index " + std::to_string(cur_index);
-            }
-            V();
-
-            value_t x = values.top();
-            values.pop();
-            values.push(cos(x));
-        } else if (s[cur_index] == 't') {
-            if (s[++cur_index] != 'g') {
-                throw "unknown operation at index " + std::to_string(cur_index);
-            }
-            if (s[++cur_index] != '(') {
-                throw "parenthesis not found at index " + std::to_string(cur_index);
-            }
-            V();
-
-            value_t x = values.top();
-            values.pop();
-            values.push(1 / tan(x));
-        } else {
-            throw "unknown operation at index " + std::to_string(cur_index);
-        }
-    } else if (s[cur_index] == 't') { // tg
-        if (s[++cur_index] != 'g') {
-            throw "unknown operation at index " + std::to_string(cur_index);
-        }
-        if (s[++cur_index] != '(') {
-            throw "parenthesis not found at index " + std::to_string(cur_index);
-        }
-        V();
-
-        value_t x = values.top();
-        values.pop();
-        values.push(tan(x));
-    } else if (s[cur_index] == 'a') { // asin, acos, atg
-        cur_index++;
-        if (s[cur_index] == 's') {
-            if (s[++cur_index] != 'i') {
-                throw "unknown operation at index " + std::to_string(cur_index);
-            }
-            if (s[++cur_index] != 'n') {
-                throw "unknown operation at index " + std::to_string(cur_index);
-            }
-            if (s[++cur_index] != '(') {
-                throw "parenthesis not found at index " + std::to_string(cur_index);
-            }
-            V();
-
-            value_t x = values.top();
-            values.pop();
-            values.push(asin(x));
-        } else if (s[cur_index] == 'c') {
-            if (s[++cur_index] != 'o') {
-                throw "unknown operation at index " + std::to_string(cur_index);
-            }
-            if (s[++cur_index] != 's') {
-                throw "unknown operation at index " + std::to_string(cur_index);
-            }
-            if (s[++cur_index] != '(') {
-                throw "parenthesis not found at index " + std::to_string(cur_index);
-            }
-            V();
-
-            value_t x = values.top();
-            values.pop();
-            values.push(acos(x));
-        } else if (s[cur_index] == 't') {
-            if (s[++cur_index] != 'g') {
-                throw "unknown operation at index " + std::to_string(cur_index);
-            }
-            if (s[++cur_index] != '(') {
-                throw "parenthesis not found at index " + std::to_string(cur_index);
-            }
-            V();
-
-            value_t x = values.top();
-            values.pop();
-            values.push(atan(x));
-        } else {
-            throw "unknown operation at index " + std::to_string(cur_index);
-        }
-    } else if (s[cur_index] == 'l') { // ln, lg
-        cur_index++;
-        if (s[cur_index] == 'n') {
-            if (s[++cur_index] != '(') {
-                throw "parenthesis not found at index " + std::to_string(cur_index);
-            }
-            V();
-
-            value_t x = values.top();
-            values.pop();
-            values.push(log(x));
-        } else if (s[cur_index] == 'g') {
-            if (s[++cur_index] != '(') {
-                throw "parenthesis not found at index " + std::to_string(cur_index);
-            }
-            V();
-
-            value_t x = values.top();
-            values.pop();
-            values.push(log10(x));
-        } else {
-            throw "unknown operation at index " + std::to_string(cur_index);
-        }
-    } else if (s[cur_index] == 'e') { // exp
-        if (s[++cur_index] != 'x') {
-            throw "unknown operation at index " + std::to_string(cur_index);
-        }
-        if (s[++cur_index] != 'p') {
-            throw "unknown operation at index " + std::to_string(cur_index);
-        }
-        if (s[++cur_index] != '(') {
-            throw "parenthesis not found at index " + std::to_string(cur_index);
-        }
-        V();
-
-        value_t x = values.top();
-        values.pop();
-        values.push(exp(x));
-    } else if (s[cur_index] == '(') {
+    }
+        // Аналогично обрабатываются другие функции
+        // и скобки с унарным минусом
+    else if (s[cur_index] == '(') {
         cur_index++;
         bool unary_minus = false;
         if (s[cur_index] == '-') {
             cur_index++;
             unary_minus = true;
         }
-        S();
+        S(); // Рекурсивно обрабатываем выражение в скобках
         if (s[cur_index] != ')') {
             throw "unclosed parenthesis at index " + std::to_string(cur_index);
         }
         if (unary_minus) {
             value_t x = values.top();
             values.pop();
-            values.push(-x);
+            values.push(-x); // Применяем унарный минус
         }
         cur_index++;
     } else {
-        D();
+        D(); // Обработка чисел
     }
 }
 
+// Обработка чисел (целых и дробных)
 void ExpressionParser::D() {
     if (!isdigit(s[cur_index])) {
         throw "expected number at index " + std::to_string(cur_index);
@@ -298,12 +186,13 @@ void ExpressionParser::D() {
     values.push(res);
 }
 
+// Основной метод для парсинга и определения выражения
 long double ExpressionParser::parse(const std::string &s_) {
-    s = "(" + s_ + ")";
+    s = "(" + s_ + ")"; // добавлю скобки для упрощения парсинга
     replace_all(s, " ", "");
     cur_index = 0;
 
-    S();
+    S(); // Начинаем парсинг
 
     if (cur_index != s.size()) {
         throw "incorrect expression";
--- ExpressionParser.h
+++ ExpressionParser.h
@@ -4,16 +4,18 @@
 #include <string>
 #include "calc.h"
 
+// парсер и определитель математических выражений
 class ExpressionParser {
     size_t cur_index;
     std::string s;
-    std::stack <value_t> values;
+    std::stack<value_t> values;  // для хранения промежуточных значений вычислений
 
-    void S(); // sum
-    void M(); // multiply
-    void P(); // power
-    void V(); // value
-    void D(); // digit
+    // методы для обработки:
+    void S(); // сложения и вычитания
+    void M(); // умножения и деления
+    void P(); // возведения в степень
+    void V(); // значений (функции, скобки, числа)
+    void D(); // цифр (целых и дробных чисел)
 public:
     long double parse(const std::string &s_);
 };
